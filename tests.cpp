4738afc2fc01982eae38b1dfc4dbb1e25a21f768
Félix Castillon <49310803+BorsukUlamThm@users.noreply.github.com>
2021-06-27 11:03:16 +0200

    CMake new setup for sources

---
+++ tests.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "src/Graphics/View/View.h"
+#include "src/Algorithms/Model/Point_2.h"
+#include "src/Algorithms/Model/Polygon_2.h"
+
+void main1()
+{
+    gr::Point a(0, 0);
+    gr::Point b(0, 1);
+    gr::Point c(1, 0);
+    gr::Point d(1, 1);
+    gr::Segment s(a, d);
+
+    gr::Figure figure;
+    figure.add_point(a);
+    figure.add_point(b);
+    figure.add_point(c);
+    figure.add_point(d);
+    figure.add_segment(a, d);
+    figure.add_segment(b, c);
+    figure.add_text("a", a);
+    figure.add_text("b", b);
+    figure.add_text("c", c);
+    figure.add_text("d", d);
+    figure.add_text("segment", s);
+
+    gr::Canvas canvas;
+    canvas.display_figure(figure);
+}
+
+int main()
+{
+    main1();
+
+    return 0;
+}
+
