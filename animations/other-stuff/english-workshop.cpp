f1e6718bfd7699301314b6d47920206114ace742
felix <felix.castillon@free.fr>
2022-11-11 07:32:23 +0100

    workshop gave a lot of new fractals

--- animations/other-stuff/english-workshop.cpp
+++ animations/other-stuff/english-workshop.cpp
@@ -23,20 +23,134 @@ namespace Other_stuff_english_workshop
 		NB_FIGS
 	};
 
-	complex f1(complex z)
+	complex s1(complex z)
 	{
 		return z / 2.0f;
 	}
 
-	complex f2(complex z)
+	complex s2(complex z)
 	{
 		return z / 2.0f + 1.0f / 2.0f;
 	}
 
-	complex f3(complex z)
+	complex s3(complex z)
 	{
 		return z / 2.0f + std::exp(i * pi / 3.0f) / 2.0f;
 	}
+
+    complex r1(complex z)
+    {
+        return z / 2.0f;
+    }
+
+    complex r2(complex z)
+    {
+        return (z + 1.0f) / 2.0f;
+    }
+
+    complex r3(complex z)
+    {
+        return (z + i) / 2.0f;
+    }
+
+    complex r4(complex z)
+    {
+        return (z + i + 1.0f) / 2.0f;
+    }
+
+    complex vk1(complex z)
+    {
+        return z / 3.0f;
+    }
+
+    complex vk2(complex z)
+    {
+        return (z + 2.0f) / 3.0f;
+    }
+
+    complex vk3(complex z)
+    {
+        return (z * std::exp(i * pi / 3.0f) + 1.0f) / 3.0f;
+    }
+
+    complex vk4(complex z)
+    {
+        return (z * std::exp(-i * pi / 3.0f) + 1.0f + std::exp(i * pi / 3.0f)) / 3.0f;
+    }
+
+    complex p1(complex z)
+    {
+        //return (z + std::exp((2.0f * i * pi) / 5.0f)) / 2.0f;
+        return (z + std::exp((3.0f * i * pi) / 5.0f)) / 2.0f;
+    }
+
+    complex p2(complex z)
+    {
+        return (z + std::exp((6.0f * i * pi) / 5.0f)) / 3.0f;
+    }
+
+    complex p3(complex z)
+    {
+        return (z + std::exp((9.0f * i * pi) / 5.0f)) / 4.0f;
+    }
+
+    complex p4(complex z)
+    {
+        return (z + std::exp((12.0f * i * pi) / 5.0f)) / 5.0f;
+    }
+
+    complex p5(complex z)
+    {
+        return (z + std::exp((15.0f * i * pi) / 5.0f)) / 6.0f;
+    }
+
+    complex truc(complex z)
+    {
+        return std::sin(z) / 2.0f;
+    }
+
+    complex machin(complex z)
+    {
+        return std::sin(2.0f * z) / 4.0f;
+    }
+
+    complex d1(complex z)
+    {
+        return z * (1.0f + i) / 2.0f;
+    }
+
+    complex d2(complex z)
+    {
+        return 1.0f - z * (1.0f - i) / 2.0f;
+    }
+
+    complex f1(complex z)
+    {
+        real x = 0;
+        real y = 0.16 * z.imag();
+        return x + y * i;
+    }
+
+    complex f2(complex z)
+    {
+        real x = 0.85 * z.real() + 0.04 * z.imag();
+        real y = -0.04 * z.real() + 0.85 * z.imag() + 1.6;
+        return x + y * i;
+    }
+
+    complex f3(complex z)
+    {
+        real x = 0.2 * z.real() - 0.26 * z.imag();
+        real y = 0.23 * z.real() + 0.22 * z.imag() + 1.6;
+        return x + y * i;
+    }
+
+    complex f4(complex z)
+    {
+        real x = -0.15 * z.real() + 0.28 * z.imag();
+        real y = 0.26 * z.real() + 0.24 * z.imag() + 0.44f;
+        return x + y * i;
+    }
 
 	compact iterations(gr::Animation& animation, bool colors)
 	{
@@ -60,12 +174,24 @@ namespace Other_stuff_english_workshop
 				auto col = gr::DEFAULT_SHAPE_COLOR;
 				if (colors)
 				{
-					if (j % 3 == 0)
-					{ col = gr::RED; }
-					else if (j % 3 == 1)
-					{ col = gr::BLUE; }
-					else
-					{ col = gr::YELLOW; }
+                    switch(j % 4)
+                    {
+                        case 0:
+                            col = gr::RED;
+                            break;
+                        case 1:
+                            col = gr::BLUE;
+                            break;
+                        case 2:
+                            col = gr::GREEN;
+                            break;
+                        case 3:
+                            col = gr::YELLOW;
+                            break;/*
+                        case 4:
+                            col = gr::PURPLE;
+                            break;*/
+                    }
 				}
 
 				auto x = gr::Coordinate(K[j].real());
@@ -78,9 +204,10 @@ namespace Other_stuff_english_workshop
 			buffer.clear();
 			for (auto& z : K)
 			{
-				buffer.push_back(f1(z));
-				buffer.push_back(f2(z));
-				buffer.push_back(f3(z));
+                buffer.push_back(f1(z));
+                buffer.push_back(f2(z));
+                buffer.push_back(f3(z));
+                buffer.push_back(f4(z));
 			}
 
 			std::swap(K, buffer);
@@ -101,7 +228,7 @@ namespace Other_stuff_english_workshop
 			std::cout << "u" << n << " = "
 					  << z.real() << " + "
 					  << z.imag() << "i" << std::endl;
-			z = f2(z);
+			z = s2(z);
 		}
 	}
 
@@ -127,7 +254,7 @@ int main(int argc, char** argv)
 {
 	using namespace Other_stuff_english_workshop;
 
-	if (argc > 1 && argv[1] == std::string("???"))
+	if (argc > 1)
 	{ ifs(argc > 2 && argv[2] == std::string("-color")); }
 	else
 	{ iterated_f2(); }
